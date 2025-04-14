fb7959e3c3a7e79633b696f206bd2e56cbe26863
Abhishek Tiwari <revilabhi@gmail.com>
2025-04-14 23:00:44 +0300

    Add files via upload

---
+++ yatube_api/api/serializers.py
@@ -0,0 +1,57 @@
+from rest_framework import serializers
+from rest_framework.relations import SlugRelatedField
+from rest_framework.validators import UniqueTogetherValidator
+
+
+from posts.models import Comment, Post, Group, Follow, User
+
+
+class GroupSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        fields = ('id', 'title', 'slug', 'description')
+        model = Group
+
+
+class PostSerializer(serializers.ModelSerializer):
+    author = SlugRelatedField(slug_field='username', read_only=True)
+
+    class Meta:
+        fields = ('id', 'text', 'pub_date', 'author', 'image', 'group')
+        model = Post
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        read_only=True, slug_field='username'
+    )
+
+    class Meta:
+        fields = ('id', 'author', 'post', 'text', 'created')
+        model = Comment
+        read_only_fields = ('post',)
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    user = SlugRelatedField(
+        read_only=True, default=serializers.CurrentUserDefault(),
+        slug_field='username'
+    )
+    following = SlugRelatedField(
+        slug_field='username', queryset=User.objects.all())
+
+    class Meta:
+        fields = ('id', 'user', 'following')
+        model = Follow
+
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Follow.objects.all(),
+                fields=('user', 'following')
+            )
+        ]
+
+    def validate_following(self, obj):
+        if obj == self.context['request'].user:
+            raise serializers.ValidationError()
+        return obj
